8c6ac21dbf2d2aa9c5e273bd3ae70f5f5925e338
Salman <salzahir18@gmail.com>
2024-12-23 15:13:31 -0600

    Made Struct for Grid and structs for grid buttons

--- edutainment/ContentView.swift
+++ edutainment/ContentView.swift
@@ -72,6 +72,82 @@ struct ScoreTitle: View {
     }
 }
 
+
+struct GridView: View {
+    
+    let items = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] // Define number of columns
+    let columns = Array(repeating: GridItem(.flexible(minimum: 25), spacing: 2), count: 3)
+    @Binding var userInput: String
+    
+    var body: some View {
+        VStack{
+            LazyVGrid(columns: columns, spacing: 2) {
+                ForEach(items, id: \.self) { item in
+                    Button(action: {
+                        userInput += String(item)
+                    }){
+                        // Gridbutton View
+                        GridButton(item: item)
+
+                    }
+                }
+            }
+            
+            HStack {
+                Button(action: {
+                    userInput += "0"
+                }) {
+                    HorizontalButton(item: "0")
+                }
+                
+                Button(action: {
+                    if !userInput.isEmpty {
+                        userInput.removeLast()
+                    }
+                }) {
+                    HorizontalButton(item: "⬅️")
+                }
+                .onLongPressGesture(minimumDuration: 1.0){
+                    userInput = ""
+                }
+                
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+struct GridButton: View {
+    var item: String
+    
+    var body: some View {
+        
+        RoundedRectangle(cornerRadius: 2)
+            .fill(Color.orange.secondary)
+            .aspectRatio(1, contentMode: .fit)
+            .overlay(
+                Text("\(item)")
+                    .foregroundColor(.black)
+            )
+    }
+}
+
+struct HorizontalButton: View {
+    var item: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 2)
+            .fill(Color.orange.secondary)
+            .frame(maxWidth: .infinity)
+            .frame(height: 50)
+            .overlay(
+                Text("\(item)")
+                    .foregroundColor(.black)
+            )
+    }
+}
+
+
 // Enscapulated MainView logic
 struct MainGameView: View {
     
@@ -85,12 +161,6 @@ struct MainGameView: View {
     @Binding var skips: Int
     var processAnswer: (Bool) -> Void
     var playAgain: () -> Void
-    
-    
-    let items = Array(1...9)
-       
-       // Define number of columns
-    let columns = Array(repeating: GridItem(.flexible(minimum: 25), spacing: 2), count: 3)
     
     var body: some View {
         VStack{
@@ -141,58 +211,8 @@ struct MainGameView: View {
                   
                 }
                 
-                LazyVGrid(columns: columns, spacing: 2) {
-                    ForEach(items, id: \.self) { item in
-                        Button(action: {
-                            userInput += String(item)
-                        }) {
-                            RoundedRectangle(cornerRadius: 2)
-                                .fill(Color.green.opacity(0.2))
-                                .aspectRatio(1, contentMode: .fit)
-                                .overlay(
-                                    Text("\(item)")
-                                        .foregroundColor(.black)
-                                )
-                        }
-                    }
-                }
-            
-                HStack {
-                    Button(action: {
-                        userInput += "0"
-                    }) {
-                        RoundedRectangle(cornerRadius: 2)
-                            .fill(Color.green.opacity(0.2))
-                            .frame(maxWidth: .infinity)  // Add this
-                            .frame(height: 50)
-                            .overlay(
-                                Text("0")
-                                    .foregroundColor(.black)
-                            )
-                    }
-                    
-                    Button(action: {
-                        if !userInput.isEmpty {
-                            userInput.removeLast()
-                        }
-                    }) {
-                        RoundedRectangle(cornerRadius: 2)
-                            .fill(Color.green.opacity(0.2))
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 50)
-
-                            .overlay(
-                                Text("X")
-                                    .foregroundColor(.black)
-                            )
-                    }
-                    .onLongPressGesture(minimumDuration: 1.0){
-                        userInput = ""
-                    }
-                                        
-                }
-                .frame(maxWidth: .infinity)
-                        
+                GridView(userInput: $userInput)
+               
             }
             
         }
@@ -445,3 +465,7 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+
+
