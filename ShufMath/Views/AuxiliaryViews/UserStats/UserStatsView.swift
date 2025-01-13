e68128f3d3da99f86ab5ca1c3623433c4f583982
Salman <salzahir18@gmail.com>
2025-01-13 11:57:10 -0600

    Minor changes on user stats view

--- ShufMath/Views/AuxiliaryViews/UserStats/UserStatsView.swift
+++ ShufMath/Views/AuxiliaryViews/UserStats/UserStatsView.swift
@@ -12,63 +12,59 @@ struct UserStatsView: View {
     @ObservedObject var viewModel: GameViewModel
     
     var body: some View {
-        VStack{
-            Section(){
-                Text("Player's LifeStats")
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .bold()
-                    .padding(.bottom, 15)
-                
-                ScrollView{
-                    VStack(spacing: 25){
-                        Text("Player has played \(userStats.gamesPlayed) games")
-                            .userViewModifier(
-                                backgroundColor: Color.green,
-                                label: "Amount of games user has played",
-                                hint: "This is the total number of games the player has played so far."
-                            )
-                        
-                        Text("Player has won \(userStats.gamesWon) games")
-                            .userViewModifier(
-                                backgroundColor: Color.blue,
-                                label: "Games Won",
-                                hint: "Total amount of games the player has won"
-                            )
-                                                
-                        Text("Player has lost \(userStats.gamesLost) games")
-                            .userViewModifier(
-                                backgroundColor: Color.yellow,
-                                label: "Games Lost",
-                                hint: "The tota amount of games the player has lost"
-                            )
-                        
-                        Text("Player average score is \(String(format: "%.2f", userStats.averageScore))")
-                            .userViewModifier(
-                                backgroundColor: Color.cyan,
-                                label: "Average score",
-                                hint: "Average score from all the games the player has played"
-                            )
-                        
-                        Text("Player perfect games is \(userStats.perfectGames)")
-                            .userViewModifier(
-                                backgroundColor: Color.indigo,
-                                label: "Perfect Games",
-                                hint: "Amount of perfect games the player has earned"
-                            )
-                        
-
-                    }
-                    .padding()
+        
+        VStack {
+            Text("Player's LifeStats")
+                .foregroundColor(.white)
+                .font(.title)
+                .bold()
+                .padding(.bottom, 15)
+            
+            ScrollView{
+                VStack(spacing: 25){
+                    Text("Player has played \(userStats.gamesPlayed) games")
+                        .userViewModifier(
+                            backgroundColor: Color.green,
+                            label: "Amount of games user has played",
+                            hint: "This is the total number of games the player has played so far."
+                        )
+                    
+                    Text("Player has won \(userStats.gamesWon) games")
+                        .userViewModifier(
+                            backgroundColor: Color.blue,
+                            label: "Games Won",
+                            hint: "Total amount of games the player has won"
+                        )
+                    
+                    Text("Player has lost \(userStats.gamesLost) games")
+                        .userViewModifier(
+                            backgroundColor: Color.yellow,
+                            label: "Games Lost",
+                            hint: "The tota amount of games the player has lost"
+                        )
+                    
+                    Text("Player average score is \(String(format: "%.2f", userStats.averageScore))")
+                        .userViewModifier(
+                            backgroundColor: Color.cyan,
+                            label: "Average score",
+                            hint: "Average score from all the games the player has played"
+                        )
+                    
+                    Text("Player perfect games is \(userStats.perfectGames)")
+                        .userViewModifier(
+                            backgroundColor: Color.indigo,
+                            label: "Perfect Games",
+                            hint: "Amount of perfect games the player has earned"
+                        )
                 }
-                
+                .padding()
+            }
+            .padding(.horizontal)
+            .onAppear() {
+                viewModel.loadUserData()
             }
         }
-        .onAppear() {
-            viewModel.loadUserData()
-        }
+        .padding(.top)
+
     }
 }
-
-
-
